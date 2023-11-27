0dd1b680890fd06f62fb70040c87aa6f97de48b0
Jmillamena <jcmillamena@gmail.com>
2023-11-26 21:12:21 -1000

    edits register form

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,9 +1,92 @@
-// Import necessary components from react-bootstrap
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import { Formik, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
+// import React from "react";
+// import axios from "axios";
+// import { useHistory } from "react-router-dom";
+// import { Formik, Field, Form, ErrorMessage } from "formik";
+// import * as Yup from "yup";
+
+// const RegisterSchema = Yup.object().shape({
+//   name: Yup.string().required("Name is required"),
+//   email: Yup.string().email("Invalid email").required("Email is required"),
+//   username: Yup.string().required("Username is required"),
+//   password: Yup.string()
+//     .min(6, "Password must be at least 6 characters")
+//     .required("Password is required"),
+// });
+
+// const Register = () => {
+//   const history = useHistory();
+
+//   const handleSubmit = async (values, { setSubmitting, setFieldError }) => {
+//     try {
+//       const response = await axios.post("http://127.0.0.1:5555/users", values);
+
+//       if (response.status === 201) {
+//         // Registration successful, you can redirect to login or any other page
+//         history.push("/login");
+//       } else {
+//         setFieldError("password", "Registration failed");
+//       }
+//     } catch (error) {
+//       console.error("Registration error", error);
+//       setFieldError("password", "Registration error");
+//     } finally {
+//       setSubmitting(false);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2>Register</h2>
+//       <Formik
+//         initialValues={{
+//           name: "",
+//           email: "",
+//           username: "",
+//           password: "",
+//         }}
+//         validationSchema={RegisterSchema}
+//         onSubmit={handleSubmit}
+//       >
+//         {({ isSubmitting }) => (
+//           <Form>
+//             <div>
+//               <label htmlFor="name">Name:</label>
+//               <Field type="text" id="name" name="name" />
+//               <ErrorMessage name="name" component="div" />
+//             </div>
+//             <div>
+//               <label htmlFor="email">Email:</label>
+//               <Field type="text" id="email" name="email" />
+//               <ErrorMessage name="email" component="div" />
+//             </div>
+//             <div>
+//               <label htmlFor="username">Username:</label>
+//               <Field type="text" id="username" name="username" />
+//               <ErrorMessage name="username" component="div" />
+//             </div>
+//             <div>
+//               <label htmlFor="password">Password:</label>
+//               <Field type="password" id="password" name="password" />
+//               <ErrorMessage name="password" component="div" />
+//             </div>
+//             <button type="submit" disabled={isSubmitting}>
+//               Register
+//             </button>
+//           </Form>
+//         )}
+//       </Formik>
+//     </div>
+//   );
+// };
+
+// export default Register;
+
+import React from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { FormGroup, FormLabel, FormControl, Button } from "react-bootstrap";
 
 const RegisterSchema = Yup.object().shape({
   name: Yup.string().required("Name is required"),
@@ -36,87 +119,87 @@ const Register = () => {
   };
 
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h2>Register</h2>
-          <Formik
-            initialValues={{
-              name: "",
-              email: "",
-              username: "",
-              password: "",
-            }}
-            validationSchema={RegisterSchema}
-            onSubmit={handleSubmit}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <Form.Group>
-                  <Form.Label>Name:</Form.Label>
-                  <Field
-                    type="text"
-                    id="name"
-                    name="name"
-                    className="form-control"
-                  />
-                  <ErrorMessage
-                    name="name"
-                    component="div"
-                    className="text-danger"
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Email:</Form.Label>
-                  <Field
-                    type="text"
-                    id="email"
-                    name="email"
-                    className="form-control"
-                  />
-                  <ErrorMessage
-                    name="email"
-                    component="div"
-                    className="text-danger"
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Username:</Form.Label>
-                  <Field
-                    type="text"
-                    id="username"
-                    name="username"
-                    className="form-control"
-                  />
-                  <ErrorMessage
-                    name="username"
-                    component="div"
-                    className="text-danger"
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Password:</Form.Label>
-                  <Field
-                    type="password"
-                    id="password"
-                    name="password"
-                    className="form-control"
-                  />
-                  <ErrorMessage
-                    name="password"
-                    component="div"
-                    className="text-danger"
-                  />
-                </Form.Group>
-                <Button type="submit" disabled={isSubmitting}>
-                  Register
-                </Button>
-              </Form>
-            )}
-          </Formik>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <h2>Register</h2>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          username: "",
+          password: "",
+        }}
+        validationSchema={RegisterSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <FormGroup>
+              <FormLabel>Name:</FormLabel>
+              <Field
+                as={FormControl}
+                type="text"
+                id="name"
+                name="name"
+                aria-describedby="name-help"
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className="text-danger"
+              />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Email:</FormLabel>
+              <Field
+                as={FormControl}
+                type="text"
+                id="email"
+                name="email"
+                aria-describedby="email-help"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="text-danger"
+              />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Username:</FormLabel>
+              <Field
+                as={FormControl}
+                type="text"
+                id="username"
+                name="username"
+                aria-describedby="username-help"
+              />
+              <ErrorMessage
+                name="username"
+                component="div"
+                className="text-danger"
+              />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Password:</FormLabel>
+              <Field
+                as={FormControl}
+                type="password"
+                id="password"
+                name="password"
+                aria-describedby="password-help"
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="text-danger"
+              />
+            </FormGroup>
+            <Button type="submit" disabled={isSubmitting}>
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
