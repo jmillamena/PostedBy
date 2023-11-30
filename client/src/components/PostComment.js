23995d1f2ec6102ee6b59d3bfe50afefa34bf31d
Jmillamena <jcmillamena@gmail.com>
2023-11-30 09:20:58 -1000

    alert no empty comments with bootstrap

--- client/src/components/PostComment.js
+++ client/src/components/PostComment.js
@@ -1,42 +1,46 @@
-//
-//with success message
+//auto update comment
+
+// PostComment.js
+
 // import React, { useState } from "react";
 // import axios from "axios";
 // import { Alert } from "react-bootstrap";
 
 // const PostComment = ({ post_id, isAuthenticated, onCommentSubmit }) => {
 //   const [comment, setComment] = useState("");
-//   const [successMessage, setSuccessMessage] = useState(null); // New state for success message
+//   const [successMessage, setSuccessMessage] = useState(null);
 
 //   const handleCommentSubmit = async () => {
 //     if (!isAuthenticated) {
-//       // Handle the case where the user is not authenticated
 //       console.log(
 //         "User is not authenticated. Redirect to login or show a message."
 //       );
 //       return;
 //     }
+//     if (comment.trim() === "") {
+//       console.log("Comment cannot be blank.");
+//       window.alert("Comment must not be blank.");
+
+//       return;
+//     }
 
 //     try {
-//       // Make a POST request to your API endpoint for adding comments
 //       await axios.post(
 //         `/postcomment/${post_id}`,
 //         { content: comment },
 //         {
 //           headers: {
-//             Authorization: `Bearer ${localStorage.getItem("access_token")}`, // Include the authorization token
+//             Authorization: `Bearer ${localStorage.getItem("access_token")}`,
 //           },
 //         }
 //       );
 
-//       // Clear the comment input after submission
 //       setComment("");
-
-//       // Set success message
 //       setSuccessMessage("Comment added successfully");
 
-//       // Optionally, you can update the state or trigger a refresh of comments
-//       // e.g., by fetching the updated comments and updating the component state
+//       if (onCommentSubmit) {
+//         onCommentSubmit(post_id);
+//       }
 //     } catch (error) {
 //       console.error("Error adding comment:", error);
 //     }
@@ -65,9 +69,7 @@
 
 // export default PostComment;
 
-//auto update comment
-
-// PostComment.js
+//changes with react
 
 import React, { useState } from "react";
 import axios from "axios";
@@ -76,12 +78,19 @@ import { Alert } from "react-bootstrap";
 const PostComment = ({ post_id, isAuthenticated, onCommentSubmit }) => {
   const [comment, setComment] = useState("");
   const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const handleCommentSubmit = async () => {
     if (!isAuthenticated) {
       console.log(
         "User is not authenticated. Redirect to login or show a message."
       );
+      return;
+    }
+
+    // Check if the comment is blank
+    if (comment.trim() === "") {
+      setErrorMessage("Comment must not be blank.");
       return;
     }
 
@@ -118,6 +127,17 @@ const PostComment = ({ post_id, isAuthenticated, onCommentSubmit }) => {
           {successMessage}
         </Alert>
       )}
+
+      {errorMessage && (
+        <Alert
+          variant="danger"
+          onClose={() => setErrorMessage(null)}
+          dismissible
+        >
+          {errorMessage}
+        </Alert>
+      )}
+
       <textarea
         value={comment}
         onChange={(e) => setComment(e.target.value)}
